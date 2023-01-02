d2ee83ba844107d1df44a90704e7835159383c20
BartlomiejSwiech <bartlomiejswiech99@gmail.com>
2023-01-02 21:11:53 +0100

    change name from App to Hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Router from './static/router';
 
-export const DOMINIK: React.FC = () => <Router />;
+export const Hello: React.FC = () => <Router />;
 
-export default DOMINIK;
+export default Hello;
