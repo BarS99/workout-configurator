5f6f1f310705100b494199c8c6e5065af6bd1dfc
BartlomiejSwiech <bartlomiejswiech99@gmail.com>
2023-01-31 22:27:53 +0100

    issue-4: Implement container component

--- src/application/core/layouts/main-layout/MainLayout.tsx
+++ src/application/core/layouts/main-layout/MainLayout.tsx
@@ -1,9 +1,13 @@
 import styles from './MainLayout.module.scss';
 import { Outlet } from 'react-router-dom';
+import Container from '../../atomic-components/container/Container';
+import { ContainerSize } from '../../models/Container.model';
 
 export const MainLayout = (): JSX.Element => (
-    <div className={styles.layout}>
-        <Outlet />
+    <div className={styles['main-layout']}>
+        <Container size={ContainerSize.XL}>
+            <Outlet />
+        </Container>
     </div>
 );
 
