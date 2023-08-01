c5bb4f2eeb6631d5b6635548bbdbbb608862f68d
Dominik <dominik.olbrycht@gmail.com>
2023-08-01 16:22:54 +0200

    style horizontal card

--- src/components/HorizontalCard.tsx
+++ src/components/HorizontalCard.tsx
@@ -1,62 +1,75 @@
-import * as React from 'react';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { deletePlan } from '../store/reducers/trainingPlanReducer';
 
 import { Card, CardHeader, CardContent, Collapse, Typography, Box, CardActions, Button } from '@mui/material/';
-
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-import { useTranslation } from 'react-i18next';
 import CrudPopover from './CrudPopover';
 import { TrainingPlan } from '../types/training-plan';
+import { hover } from '@testing-library/user-event/dist/hover';
+import { Link } from 'react-router-dom';
 
 const HorizontalCard = (props: TrainingPlan): JSX.Element => {
     const { id, name, desc, days } = props;
+
     const { t } = useTranslation();
     const dispatch = useDispatch();
-    const [expanded, setExpanded] = React.useState(false);
+
+    const [expanded, setExpanded] = useState(false);
 
     const handleExpandClick = (): void => {
         setExpanded(!expanded);
     };
 
     const handleEdit = (): void => {};
+
     const handleDelete = (): void => {
         dispatch(deletePlan(props));
     };
 
-    return (
-        <Card sx={{ p: 2, mb: 2 }}>
-            <CardHeader
-                sx={{ p: 0 }}
-                action={
-                    <Box>
-                        <CrudPopover handleEdit={handleEdit} handleDelete={handleDelete} />
-                    </Box>
-                }
-                title={name}
-                subheader=""
-            />
+    const cardStyle = {
+        p: 2,
+        mb: 2,
+        '  &:hover': { backgroundColor: '#eee', cursor: 'pointer' }
+    };
 
-            <CardActions>
-                <Box component={Button} variant="text" size="small" onClick={handleExpandClick}>
-                    <Typography variant="subtitle2">{t('common.showMore')}</Typography>
-                    <ExpandMoreIcon
-                        style={{
-                            color: 'black',
-                            display: 'flex',
-                            transform: `rotate(${expanded ? '180' : '0'}deg)`,
-                            transition: 'transform 0.3s ease'
-                        }}
-                    />
-                </Box>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography>{desc}</Typography>
-                </CardContent>
-            </Collapse>
-        </Card>
+    return (
+        <Link to={'/creator/' + id}>
+            <Card sx={cardStyle}>
+                <CardHeader
+                    sx={{ p: 0 }}
+                    action={
+                        <Box>
+                            <CrudPopover handleEdit={handleEdit} handleDelete={handleDelete} />
+                        </Box>
+                    }
+                    title={name}
+                    subheader=""
+                />
+
+                <CardActions>
+                    <Box component={Button} variant="text" size="small" onClick={handleExpandClick}>
+                        <Typography variant="subtitle2">{t('common.showMore')}</Typography>
+                        <ExpandMoreIcon
+                            style={{
+                                zIndex: '10',
+                                color: 'black',
+                                display: 'flex',
+                                transform: `rotate(${expanded ? '180' : '0'}deg)`,
+                                transition: 'transform 0.3s ease'
+                            }}
+                        />
+                    </Box>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography>{desc}</Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
+        </Link>
     );
 };
 
